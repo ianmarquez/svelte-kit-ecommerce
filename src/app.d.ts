0a5216fcbeb8a92377af2b5c109e6c0215935724
Ian Marquez <regilmarquez123@gmail.com>
2023-08-05 12:50:45 +0800

    Initial Commit

---
+++ src/app.d.ts
@@ -0,0 +1,15 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+declare namespace App {
+	// interface Locals {}
+	// interface PageData {}
+	// interface Error {}
+	// interface Platform {}
+}
+
+declare class Product {
+	id: string; // stripe id
+	name: string; // coffee
+	price: number; // 5
+}
---
+++ src/lib/productCard.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+	<header class="card-header">
+		<h2>Product Name</h2>
+		<div class="card-body px-4">
+			Quantity: <strong>4</strong>
+		</div>
+		<div class="card-body px-4">Price: 10$</div>
+		<footer class="card-footer">
+			<button class="p-2 rounder variant-glass-primary rounded">Add</button>
+			<button class="p-2 rounder variant-glass-error rounded">Remove</button>
+		</footer>
+	</header>
+</div>
+:
